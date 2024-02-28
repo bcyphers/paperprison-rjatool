cab4da8a6ac5347595bd31190be03a8ac945cc6a
Bennett Cyphers <bnenetcyphers@gmail.com>
2024-02-27 17:30:24 -0800

    Update to_db.py to work with new csv format

--- to_db.py
+++ to_db.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlite3
 
-def main():
+def merge_pop():
     df = pd.read_csv('tool_table_cyo_all_g.csv')
     df = df[df.gender == 'Total']
 
@@ -24,22 +24,46 @@ def main():
     df = df.rename(columns={'event': 'decision'})
     df = df.assign(county=df.county.replace('California', 'All Counties'))
     df = df.assign(year=df.year.replace('All', 'All Years'))
+    return df
 
-    with sqlite3.connect('./rja.db') as db:
-        df.to_sql('data', db)
+def main():
+    df = pd.read_csv('RJA_tool_table_02-27-2024.csv')
 
+    df = df.rename(columns={'event': 'decision', 'w_pop': 'pop_white'})
+    df = df.drop(['rate_per_100_pop', 'disparity_gap_pop_w'], axis=1)
+    df = df.assign(county=df.county.replace('California', 'All Counties'))
+    df = df.assign(year=df.year.replace('All', 'All Years'))
+
+    return df
 
 def check_rate_per_pop(df):
-    def per_100(r):
-        ratio = int(r['number']) / int(r['pop'])
-        if r['year'] == '2021':
+    def per_100(n, p, year):
+        ratio = int(n) / int(p)
+        if year == '2021':
             ratio *= 4/3
-        elif r['year'] == 'All':
+        elif year == 'All':
             ratio /= 11.75
         return ratio * 100
 
-    df['new_per_100'] = df.apply(per_100, axis=1)
-    df['ratio'] = df.apply(lambda r: r['rate_per_100_pop'] / r['new_per_100'], axis=1)
+    df['new_per_pop'] = df.apply(lambda r: per_100(r['number'], r['pop'], r['year']),
+                                 axis=1)
+    df['per_pop_ratio'] = df.apply(lambda r: r['rate_per_100_pop'] / r['new_per_pop'],
+                                   axis=1)
+    df['new_disp_gap'] = df.apply(lambda r: per_100(r['number'], r['pop'], r['year']) /
+                                      per_100(r['number_white'], r['pop_white'], r['year']),
+                                  axis=1)
+    df['disp_gap_ratio'] = df.apply(lambda r: r['disparity_gap_pop_w'] / r['new_disp_gap'],
+                                    axis=1)
+
+    print(df[(df.disp_gap_ratio - 1).abs() > 0.01])
+    print(len(df[(df.disp_gap_ratio - 1).abs() > 0.01]))
+    print(df[(df.per_pop_ratio - 1).abs() > 0.01])
+    print(len(df[(df.per_pop_ratio - 1).abs() > 0.01]))
+
+    return df
 
 if __name__ == '__main__':
-    main()
+    df = main()
+
+    with sqlite3.connect('./rja.db') as db:
+        df.to_sql('data', db)
