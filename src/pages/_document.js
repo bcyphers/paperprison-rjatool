63cd961a49b17fd6e34cfecb6405172782636106
akhilraj2895 <dustfinger2503@gmail.com>
2024-04-17 16:28:33 -0700

    Revert "updated document.js file with gtag"
    This reverts commit ea509b8e5499730a37cad11a99f99482328e9c3f.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,34 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id=GTM-MZ9Z734P'+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-MZ9Z734P');
-              `,
-            }}
-          />
-        </Head>
-        <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZ9Z734P" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument;
