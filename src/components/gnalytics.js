d77792d03857e1540db1df95c2c1a69a588f8f8f
akhilraj2895 <dustfinger2503@gmail.com>
2024-04-17 16:50:42 -0700

    Revert "ganalytics implementation"
    This reverts commit 0c281c086ae48adc4c3cd6ed16051afeac4934b7.

--- src/components/gnalytics.js
+++
@@ -1,9 +0,0 @@
-import ReactGA from "react-ga";
-
-export const initGA = () => {
-  ReactGA.initialize('G-TRXSCTQ2BC');
-};
-
-export const logPageView = () => {
-  ReactGA.pageview(window.location.pathname + window.location.search);
-};
