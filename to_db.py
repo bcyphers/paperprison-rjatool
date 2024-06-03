2ebe724d644b7085ecfb5c921c42005f6aa1d860
Bennett Cyphers <bennettcyphers@gmail.com>
2024-06-03 11:55:26 -0400

    Update to_db.py to populate the database specified by config.ini

--- to_db.py
+++ to_db.py
@@ -1,23 +1,12 @@
 import pandas as pd
-import mysql.connector
-from sqlalchemy import create_engine
-
-dtype_map = {
-    'O' : 'TEXT',
-    'object' : 'TEXT',
-    'int64' : 'INT',
-    'float64' : 'FLOAT',
-    'datetime64' : 'DATETIME',
-    'bool' : 'TINYINT',
-    'category' : 'TEXT',
-    'timedelta[ns]' : 'TEXT'
-}
+from sqlalchemy import create_engine, URL
 
 
-def main():
+def clean_csv():
     df = pd.read_csv('RJA_tool_table_04-01-2024.csv')
 
-    df = df.rename(columns={'event': 'decision', 'w_pop': 'pop_white'})
+    df = df.rename(columns={'event': 'decision', 'w_pop': 'pop_white',
+                            'offense_name': 'offense'})
     df = df.drop(['rate_per_100_pop', 'disparity_gap_pop_w'], axis=1)
     df = df.assign(county=df.county.replace('California', 'All Counties'))
     df = df.assign(year=df.year.replace('All', 'All Years'))
@@ -27,29 +16,17 @@ def main():
     return df
 
 if __name__ == '__main__':
-    df = main()
-
-    headers = ["{0} {1}".format(h, dtype_map[str(df[h].dtype)]) for h in
-               df.dtypes.index]
-    sql = "CREATE TABLE data (%s)" % ", ".join(headers)
-    print(sql)
+    df = clean_csv()
 
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    cnx = mysql.connector.connect(
-        host=config['mysqlDB']['host'],
-        user=config['mysqlDB']['user'],
+    engine = create_engine(URL.create(
+        "mysql",
+        username=config['mysqlDB']['user'],
         password=config['mysqlDB']['pass'],
-        database=config['mysqlDB']['db'])
+        host=config['mysqlDB']['host'],
+        database=config['mysqlDB']['db'],
+    ))
 
-    cur = cnx.cursor()
-    cur.execute(sql)
-    cur.close()
-    cnx.commit()
-
-    engine = create_engine("mysql://root:root@localhost:3306/rjatool")
     df.to_sql('data', engine, if_exists='replace')
-
-    #with sqlite3.connect('./rja.db') as db:
-        #df.to_sql('data', db)
