3c07bd51cc0e017a0cbe72e6ada24d79adfc2241
akhilraj2895 <dustfinger2503@gmail.com>
2024-07-06 15:59:59 -0700

    Header rendering issue fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,9 @@ const Header = () => {
   };
 
   const toggleDropdown = (dropdown) => {
-    setActiveDropdown(activeDropdown === dropdown ? null : dropdown);
+    if (window.innerWidth <= 845) {
+      setActiveDropdown(activeDropdown === dropdown ? null : dropdown);
+    }
   };
 
   useEffect(() => {
@@ -54,35 +56,33 @@ const Header = () => {
         <button ref={hamburgerRef} className="hamburger-menu" onClick={toggleMenu}>
           ☰
         </button>
-        {isMenuOpen && (
-          <nav ref={menuRef} className="main-nav open">
-            <span className={`nav dropbtn ${activeDropdown === 'about' ? 'active' : ''}`} onClick={() => toggleDropdown('about')}>
-              <a className="mr-5 hover:text-gray-900 nav dropbtn" href="https://paperprisons.org/about.html">About</a>
-              <div className="dropdown-content">
-                <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/SecondChanceGap.html">What is the Second Chance Gap?</a>
-                <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/team.html">Team</a>
-                <p><strong><u>Research Papers:</u> &nbsp;&nbsp;&nbsp;</strong></p>
-                <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/PaperPrisons.html">- The Second Chance Gap Paper</a>
-                <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/LossOfEarnings.html">- Loss of Earnings Paper</a>
-                <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/ProvingRacialDisparity.html">- Proving Racial Disparity Paper</a>
-                <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/RJATool.html">- RJA Data Tool Paper</a>
-              </div>
-            </span>
-            <span className={`nav dropbtn ${activeDropdown === 'news' ? 'active' : ''}`} onClick={() => toggleDropdown('news')}>
-              <a className="mr-5 hover:text-gray-900 nav dropbtn" href="https://paperprisons.org/blog.html?source=rja">News/Blog</a>
-              <div className="dropdown-content">
-                <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/news.html?source=rja">News</a>
-                <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/blog.html?source=rja">Blog</a>
-              </div>
-            </span>
-            <span className="nav dropbtn">
-              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/rja-diary.html">Diary</a>
-            </span>
-            <span className="nav dropbtn">
-              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/RJA">RJA <span className="text-gray-900">[beta]</span></a>
-            </span>
-          </nav>
-        )}
+        <nav ref={menuRef} className={`main-nav ${isMenuOpen ? 'open' : ''}`}>
+          <span className={`nav dropbtn ${activeDropdown === 'about' ? 'active' : ''}`} onClick={() => toggleDropdown('about')}>
+            <a className="mr-5 hover:text-gray-900 nav dropbtn" href="https://paperprisons.org/about.html">About</a>
+            <div className="dropdown-content">
+              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/SecondChanceGap.html">What is the Second Chance Gap?</a>
+              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/team.html">Team</a>
+              <p><strong><u>Research Papers:</u> &nbsp;&nbsp;&nbsp;</strong></p>
+              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/PaperPrisons.html">- The Second Chance Gap Paper</a>
+              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/LossOfEarnings.html">- Loss of Earnings Paper</a>
+              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/ProvingRacialDisparity.html">- Proving Racial Disparity Paper</a>
+              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/RJATool.html">- RJA Data Tool Paper</a>
+            </div>
+          </span>
+          <span className={`nav dropbtn ${activeDropdown === 'news' ? 'active' : ''}`} onClick={() => toggleDropdown('news')}>
+            <a className="mr-5 hover:text-gray-900 nav dropbtn" href="https://paperprisons.org/blog.html?source=rja">News/Blog</a>
+            <div className="dropdown-content">
+              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/news.html?source=rja">News</a>
+              <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/blog.html?source=rja">Blog</a>
+            </div>
+          </span>
+          <span className="nav dropbtn">
+            <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/rja-diary.html">Diary</a>
+          </span>
+          <span className="nav dropbtn">
+            <a className="mr-5 hover:text-gray-900 nav" href="https://paperprisons.org/RJA">RJA <span className="text-gray-900">[beta]</span></a>
+          </span>
+        </nav>
       </div>
     </div>
   );
